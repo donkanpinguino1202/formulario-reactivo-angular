3e1a94477a05787f51edc4b0c42f7a977874ab98
donkanpinguino1202 <davisitho0530@gmail.com>
2026-07-24 12:04:28 -0500

    Configuración de Reactive Forms

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
+    provideRouter(routes),
+    importProvidersFrom(ReactiveFormsModule)
+  ]
 };
